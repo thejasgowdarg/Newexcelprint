59da2250ce9fdf9ab9d5e255dbc11f9f35941f3d
Tulasikumar <Tulasikumar@Tulasikumar.Relyon.com>
2023-10-16 10:23:54 +0530

    new

---
+++ src/test/java/Demo/Demo4.java
@@ -0,0 +1,33 @@
+package Demo;
+
+import java.io.FileInputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+public class Demo4 {
+	@Test
+	
+	public void Base() throws Exception {
+	
+	String path="C:\\Users\\Tulasikumar\\eclipse-workspace\\Excel_Print\\src\\main\\java\\Book1.xlsx";
+    String sheetname="Sheet1";
+    
+    FileInputStream file=new FileInputStream(path);
+    XSSFWorkbook book=new XSSFWorkbook(file);
+    XSSFSheet sheet=book.getSheet(sheetname);
+    
+    
+    for(int i=0;i<sheet.getLastRowNum()+1;i++) {
+    	for(int j=0;j<sheet.getRow(0).getLastCellNum();j++) {
+    		String data=sheet.getRow(i).getCell(j).toString();
+    		System.out.print("   "+data);
+    	}
+    	System.out.println();
+    }
+    book.close();
+ 
+    
+}
+}
---
+++ src/test/java/Demo/Demo5.java
@@ -0,0 +1,32 @@
+package Demo;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+public class Demo5 {
+	@Test
+	
+	public void Base() throws Exception {
+		
+		String path="C:\\Users\\Tulasikumar\\eclipse-workspace\\Excel_Print\\src\\main\\java\\Book1.xlsx";
+		String sheetname="Sheet1";
+		
+		FileInputStream file=new FileInputStream(path);
+		XSSFWorkbook book=new XSSFWorkbook(file);
+		XSSFSheet sheet=book.getSheet(sheetname);
+		
+		for(int i=0;i<sheet.getLastRowNum();i++) {
+			for(int j=0;j<sheet.getRow(0).getLastCellNum();j++) {
+				String data=sheet.getRow(i).getCell(j).toString();
+				System.out.print("    "+data);
+			}
+			System.out.println();
+		}
+		book.close();
+	}
+
+}
